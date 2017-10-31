b338be35eb29deca18415b77d3f4962a08d6a54e
rocio <31254657+wuozio@users.noreply.github.com>
2017-10-31 13:01:52 -0600

    Add files via upload

---
+++ promedios.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int sum=0;
+  int edad[16];
+  for (int i=0; i<16; i++){
+    cout<<"¿que edad tienes?"<<endl;
+    cin>>edad[i];
+  }
+  for (int j=0; j<16; j++)
+    sum+=(edad[j]+3);
+  for (int j=0; j<16; j++)
+    cout<<edad [j]<<" , ";
+  cout<<endl;
+  cout<<"el promedio de la edad de nuestra felicidad grupal es "<<sum/16<<endl;
+  return 0;
+}
